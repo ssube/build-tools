a027fc471ec89e2f5f01cacdc3cded60bde22184
Sean Sube <seansube@gmail.com>
2018-02-22 07:48:09 -0600

    manage vpc gw in ansible (kops#780) and retag to render correctly

--- terraform/modules/aws/vpc/network/network.tf
+++ terraform/modules/aws/vpc/network/network.tf
@@ -1,14 +1,3 @@
 data "aws_vpc" "cluster_vpc" {
   id = "${var.vpc_id}"
 }
-
-resource "aws_internet_gateway" "cluster_gw" {
-  vpc_id        = "${data.aws_vpc.cluster_vpc.id}"
-
-  tags {
-    account     = "${var.tag_account}"
-    environment = "${var.tag_environment}"
-    owner       = "${var.tag_owner}"
-    project     = "${var.tag_project}"
-  }
-}
