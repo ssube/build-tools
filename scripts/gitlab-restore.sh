9361308c13eb4c0689630016f035ab6fa9933d9d
Sean Sube <seansube@gmail.com>
2018-03-11 16:42:47 -0500

    enable rbac, set up dashboard and runner roles, update cluster

--- scripts/gitlab-restore.sh
+++ scripts/gitlab-restore.sh
@@ -12,7 +12,7 @@ backup_src="s3://${PROJECT_NAME}-backup-prod-primary/${backup_name}_gitlab_backu
 echo "${backup_src}"
 
 # sign the s3 url
-presign_url="$(aws s3 presign "${backup_src}")"
+presign_url="$(aws s3 presign "${backup_src}" --expires-in 3600)"
 echo "${presign_url}"
 
 # restore
@@ -20,6 +20,6 @@ gitlab_pod="$(find_pod gitlab)"
 echo "running in pod '${gitlab_pod}'..."
 
 exec_in_pod "${gitlab_pod}" ls -lha $(dirname ${backup_dest})
-exec_in_pod "${gitlab_pod}" wget -d ${presign_url} -O "${backup_dest}"
+exec_in_pod "${gitlab_pod}" wget -d "${presign_url}" -O "${backup_dest}"
 exec_in_pod "${gitlab_pod}" gitlab-rake gitlab:backup:restore BACKUP="${backup_name}"
 delete_pod "${gitlab_pod}"
