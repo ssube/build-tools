9133013217f005fa966e0cfb79f8b36ac56fde8d
Sean Sube <seansube@gmail.com>
2018-04-01 19:34:28 -0500

    begin using standard colors

--- scripts/common-colors.sh
+++ scripts/common-colors.sh
@@ -8,3 +8,26 @@ function begin_color() {
 function close_color() {
   echo -ne '\e[0m';
 }
+
+function std_color() {
+  class="${1}"
+
+  case ${class} in
+    success)
+      echo 2
+      ;;
+    failure)
+      echo 1
+      ;;
+    progress)
+      echo 5
+      ;;
+    start)
+      ;&
+    end)
+      ;&
+    *)
+      echo 4
+      ;;
+  esac
+}
--- scripts/gitlab-backup.sh
+++ scripts/gitlab-backup.sh
@@ -6,13 +6,13 @@ source "$(dirname ${BASH_SOURCE[0]})/common.sh"
 # gitlab backup script
 ###
 
-begin_color 4
+begin_color $(std_color start)
 echo "Starting backup: $(date)"
 close_color
 
 gitlab_pod="$(find_pod gitlab)"
 exec_in_pod "${gitlab_pod}" gitlab-rake gitlab:backup:create SKIP=artifacts,builds,registry
 
-begin_color 4
+begin_color $(std_color success)
 echo "Finished backup: $(date)"
 close_color
